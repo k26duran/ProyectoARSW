30e237cfa74882deb2c9f61e7fc23809d2bfca88
CrkJohn <jhonhrge@gmail.com>
2019-04-09 10:37:02 -0500

    Funciona socket localhost, verificar multiples origin

--- src/main/java/edu/eci/arsw/evern/sockets/EvernControllerSockets.java
+++ src/main/java/edu/eci/arsw/evern/sockets/EvernControllerSockets.java
@@ -5,18 +5,20 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.eci.arsw.evern.model.Viaje;
+import edu.eci.arsw.evern.model.Viaje2;
 
 @Controller
-@CrossOrigin
+@RequestMapping("/")
 public class EvernControllerSockets {
-
+	
 	
 	@MessageMapping("/topic/newpoint")
     @SendTo("/topic/newpoint")
-    public Viaje sendMessage(@Payload Viaje viaje) {
-		System.out.println(viaje.toString());
+    public Viaje2 sendMessage(@Payload Viaje2 viaje) {
+		System.err.println("ACAAAAAAAA");
         return viaje;
     }
 	
--- src/main/java/edu/eci/arsw/evern/sockets/EvernSocketConfig.java
+++ src/main/java/edu/eci/arsw/evern/sockets/EvernSocketConfig.java
@@ -2,6 +2,8 @@ package edu.eci.arsw.evern.sockets;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;;;
@@ -9,6 +11,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;;;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin
 public class EvernSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
 	  
 	@Override
@@ -17,9 +20,13 @@ public class EvernSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
         config.setApplicationDestinationPrefixes("/app");
     }
 
+	
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint2").withSockJS();
-    }
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("http://localhost:5000").withSockJS();
 
+        //registry.addEndpoint("/stompendpoint").withSockJS();
+    }
+    
+   
 }

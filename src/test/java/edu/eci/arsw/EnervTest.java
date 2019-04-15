98949ca1c2071fdbdb76f9d2da396fe11d8eacaa
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-04-14 21:19:19 -0500

    dejando pom como estaba inicialmente

--- src/test/java/edu/eci/arsw/EnervTest.java
+++ src/test/java/edu/eci/arsw/EnervTest.java
@@ -15,10 +15,10 @@ import edu.eci.arsw.evern.persistence.PasajeroRepository;
 import edu.eci.arsw.evern.persistence.ViajeRepository;
 
 public class EnervTest {
-	/*
+	
 	@Test
 	public void savePasajeroConViajes() {
-		PasajeroRepository pr= new PasajeroRepository();
+		/*PasajeroRepository pr= new PasajeroRepository();
 		Pasajero p= new Pasajero("prueba5@correo", "prueba1", "prueba", "1283", "123");
 		Pasajero p1= new Pasajero("prueba4@correo", "prueba1", "prueba", "22333", "123");
 		Pasajero p2= new Pasajero("prueba3@correo", "prueba1", "prueba", "222222", "123");
@@ -31,12 +31,13 @@ public class EnervTest {
 		viajes.add(v);
 		p.setViajes(viajes);
 		assertNotNull(pr.getViajesPasajeroByCorreo(p.getCorreo()));
-		assertNotNull(pr.findAll());
+		assertNotNull(pr.findAll());*/
 		
 	}
 	
 	@Test
 	public void saveConductorConViajes() {
+		/*
 		ConductorRepository cr= new ConductorRepository();
 		Automovil auto= new Automovil("cwe341", "2018", "camioneta", "blanco");
 		Conductor c=new Conductor("coductorPrueba3@correo", "conductor3", "prueba", "88818", "123",null);
@@ -44,7 +45,7 @@ public class EnervTest {
 		assertNotNull(cr.getCondutor("coductorPrueba1@correo"));
 		assertNotNull(cr.findAll());
 		ViajeRepository vr= new ViajeRepository();
-		assertNotNull(vr.findAll());
+		assertNotNull(vr.findAll());*/
 	}
-	*/
+	
 }

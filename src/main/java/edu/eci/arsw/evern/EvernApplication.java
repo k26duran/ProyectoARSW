953af4f45afade4404fc94f10822be1454d6ddb2
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-05-08 18:49:36 -0500

    tratando de implementar sprint security
    Co-authored-by: CrkJohn <JhonhRge@gmail.com>
    Co-authored-by: k26duran <k26duran@gmail.com>

--- src/main/java/edu/eci/arsw/evern/EvernApplication.java
+++ src/main/java/edu/eci/arsw/evern/EvernApplication.java
@@ -1,36 +1,25 @@
 package edu.eci.arsw.evern;
 
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-
-
-import java.sql.Connection;
-
-import java.sql.ResultSet;
-
-import java.sql.SQLException;
-
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import edu.eci.arsw.evern.security.EvernSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import edu.eci.arsw.evern.security.EvernSecurity;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+
 @ComponentScan("edu.eci")
 public class EvernApplication {
 	
+	@Bean
+	public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+		return new EvernSecurity();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(EvernApplication.class, args);
 	}
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,92 +1,61 @@
+jQuery(document).ready(function($) {
+	showMeYourCookies('When index document is ready');
 
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+	$.ajax({
+		type: 'GET',
+		url: '/v1/pasajeros'
 
-index=(function(){
-	
-	
-	return {
-		next:(function(){
-			
-			if(animating) return false;
-			animating = true;
-			
-			current_fs = $(this).parent();
-			next_fs = $(this).parent().next();
-			
-			//activate next step on progressbar using the index of next_fs
-			$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-			
-			//show the next fieldset
-			next_fs.show(); 
-			//hide the current fieldset with style
-			current_fs.animate({opacity: 0}, {
-				step: function(now, mx) {
-					//as the opacity of current_fs reduces to 0 - stored in "now"
-					//1. scale current_fs down to 80%
-					scale = 1 - (1 - now) * 0.2;
-					//2. bring next_fs from the right(50%)
-					left = (now * 50)+"%";
-					//3. increase opacity of next_fs to 1 as it moves in
-					opacity = 1 - now;
-					current_fs.css({
-		        'transform': 'scale('+scale+')',
-		        'position': 'absolute'
-		      });
-					next_fs.css({'left': left, 'opacity': opacity});
-				}, 
-				duration: 800, 
-				complete: function(){
-					current_fs.hide();
-					animating = false;
-				}, 
-				//this comes from the custom easing plugin
-				easing: 'easeInOutBack'
-			});
+	}).done(function (data, textStatus, jqXHR) {
+		showMeYourJqXHR('When GET /v1/pasajeros is done', jqXHR);
+		showMeYourCookies('When GET /v1/pasajeros is done');
+
+		var csrfToken = jqXHR.getResponseHeader('X-CSRF-TOKEN');
+		if (csrfToken) {
+			var cookie = JSON.parse($.cookie('pasajero'));
+			cookie.csrf = csrfToken;
+			$.cookie('pasajero', JSON.stringify(cookie));
+		}
+
+	}).fail(function (jqXHR, textStatus, errorThrown) {
+		showMeYourJqXHR('When GET /v1/pasajeros fails', jqXHR);
+		showMeYourCookies('When GET /v1/pasajeros fails');
+
+		if (jqXHR.status === 401) { // HTTP Status 401: Unauthorized
+			var cookie = JSON.stringify({method: 'GET', url: '/', csrf: jqXHR.getResponseHeader('X-CSRF-TOKEN')});
+			$.cookie('pasajero', cookie);
+
+			window.location = '/login.html';
+
+		} else {
+			console.error('Houston, we have a problem...');
+		}
+	});
+
+	$('#logoutButton').on('click', function (event) {
+		event.preventDefault();
+
+		showMeYourCookies('When logoutButton is clicked');
+
+		var cookie = JSON.parse($.cookie('pasajero'));
+		$.ajax({
+			data: {},
+			headers: {'X-CSRF-TOKEN': cookie.csrf},
+			timeout: 1000,
+			type: 'POST',
+			url: '/logout'
+
+		}).done(function(data, textStatus, jqXHR) {
+			showMeYourJqXHR('When POST /logout is done', jqXHR);
+			showMeYourCookies('When POST /logout is done');
+
+			console.info('congratulations, you have logged out!');
+
+			window.location = '/';
+
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			showMeYourJqXHR('When POST /logout fails', jqXHR);
+			showMeYourCookies('When POST /logout fails');
+			console.error('Help! I cannot get out of here!');
 		});
-
-		
-	}	
-
-})();
-
-previous=(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
 	});
 });
-
-submit=(function(){
-	return false;
-})
